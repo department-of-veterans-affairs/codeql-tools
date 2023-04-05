90fbd4b39ff18aed2557ee1aad4647b671847782
Brett Logan <lindluni@github.com>
2023-04-04 21:42:22 -0400

    Add Jenkins shared libraries
    Signed-off-by: Brett Logan <lindluni@github.com>

---
+++ jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
@@ -0,0 +1,37 @@
+def call(org, repo, branch, language, buildCommand, token) {
+    sh """
+        if [ -z "$branch" ]; then
+            # This doesn't work if branch includes a slash in it
+            branch=\$(echo "${env.GIT_BRANCH}" | cut -d'/' -f2)
+        fi
+
+        databasePath="$repo-$language"
+        echo "\$databasePath"
+        codeql database init --language "$language" --source-root . --begin-tracing "\$databasePath"
+
+        echo "Executing build command: $buildCommand"
+        . "./\$databasePath/temp/tracingEnvironment/start-tracing.sh"
+        eval "$buildCommand"
+        . "./\$databasePath/temp/tracingEnvironment/end-tracing.sh"
+
+        codeql database finalize "\$databasePath"
+        sarifPath="$WORKSPACE_TMP/\$databasePath.sarif"
+        codeql database analyze "\$databasePath" "$language-code-scanning.qls" --sarif-category="$language" --format=sarif-latest --output="\$sarifPath"
+
+        commit=\$(git rev-parse HEAD)
+        GITHUB_TOKEN="$token" codeql github upload-results \
+        --repository="$org/$repo" \
+        --ref="refs/heads/$branch" \
+        --commit="\$commit" \
+        --sarif="\$sarifPath"
+
+        databaseBundle="$language-database.zip"
+        codeql database bundle "\$databasePath" --output "\$databaseBundle"
+        sizeInBytes=`stat --printf="%s" \$databaseBundle`
+        curl --http1.0 --silent --retry 3 -X POST -H "Content-Type: application/zip" \
+        -H "Content-Length: \$sizeInBytes" \
+        -H "Authorization: token $token" \
+        -T "\$databaseBundle" \
+        "https://uploads.github.com/repos/$org/$repo/code-scanning/codeql/databases/$language?name=\$databaseBundle"
+     """
+}
