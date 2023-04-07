a7905cb67d0b118606075af049cf3c245bb384b3
Brett Logan <lindluni@github.com>
2023-04-06 23:51:21 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
@@ -7,7 +7,11 @@ def call(Org, Repo, Branch, Language, BuildCommand, Token) {
 
         Write-Output "Initializing database"
         \$DatabasePath = "$Repo-$Language"
-        codeql database create "\$DatabasePath" --language "$Language" --source-root . --command "$BuildCommand"
+        if ("$BuildCommand" -eq "") {
+            codeql database create \$DatabasePath --language "$Language" --source-root .
+        } else {
+            codeql database create \$DatabasePath --language "$Language" --source-root . --command "$BuildCommand"
+        }
         Write-Output "Database initialized"
 
         Write-Output "Analyzing database"
@@ -20,7 +24,6 @@ def call(Org, Repo, Branch, Language, BuildCommand, Token) {
 
 
         Write-Output "Uploading SARIF file"
-        \$env:GITHUB_TOKEN = "$Token"
         \$Commit = "\$(git rev-parse HEAD)"
         codeql github upload-results --repository "$Org/$Repo"  --ref "refs/heads/$Branch" --commit "\$Commit" --sarif="\$DatabasePath.sarif"
         Write-Output "SARIF file uploaded"
