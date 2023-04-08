ff84334e6f7905005489403e75aaee7537a4f578
Brett Logan <lindluni@github.com>
2023-04-08 10:38:22 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
@@ -46,7 +46,7 @@ def call(org, repo, branch, language, buildCommand, token) {
         sizeInBytes=`stat --printf="%s" \$databaseBundle`
         curl --http1.0 --silent --retry 3 -X POST -H "Content-Type: application/zip" \
         -H "Content-Length: \$sizeInBytes" \
-        -H ${AUTHORIZATION_HEADER} \
+        -H "${AUTHORIZATION_HEADER}" \
         -T "\$databaseBundle" \
         "https://uploads.github.com/repos/$org/$repo/code-scanning/codeql/databases/$language?name=\$databaseBundle"
         echo "Database Bundle uploaded"
