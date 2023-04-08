63911e01a767c26c31c2ed8eda1f6dd4cb7d71f9
Brett Logan <lindluni@github.com>
2023-04-08 15:28:41 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
@@ -51,9 +51,8 @@ def call(Org, Repo, Branch, Language, BuildCommand, Token) {
             "Authorization" = "\$Env:AUTHORIZATION_HEADER"
         }
 
-        ls
-        Write-Output "-ContentType "application/zip" -Headers \$Headers -Method Post -InFile "\$Env:DATABASE_BUNDLE" -Uri "\$Env:UPLOAD_URL""
-        Invoke-RestMethod -ContentType "application/zip" -Headers \$Headers -Method Post -InFile './\$Env:DATABASE_BUNDLE' -Uri '\$Env:UPLOAD_URL'
+        Write-Output "-ContentType "application/zip" -Headers \$Headers -Method Post -InFile "./\$Env:DATABASE_BUNDLE" -Uri "\$Env:UPLOAD_URL""
+        Invoke-RestMethod -ContentType "application/zip" -Headers \$Headers -Method Post -InFile "\$(\$Env:DATABASE_BUNDLE)" -Uri "\$(\$Env:UPLOAD_URL)"
         Write-Output "Database Bundle uploaded"
     """
 
