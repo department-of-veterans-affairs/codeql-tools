b18988c908074744ad7ecf65b754f6a3ed13c45b
Brett Logan <lindluni@github.com>
2023-04-08 10:06:43 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
@@ -1,49 +1,46 @@
 def call(Org, Repo, Branch, Language, BuildCommand, Token) {
         env.TokenHeader = "token $Token"
-        powershell '''
-            Write-Output "\$Env:TokenHeader"
-        '''
 
-//     powershell """
-//         if ("$Branch" -eq "")
-//         {
-//             $Branch = "\$((Write-Output \$env:GIT_BRANCH).split('/')[1])"
-//         }
-//
-//         Write-Output "Initializing database"
-//         \$DatabasePath = "$Repo-$Language"
-//         if ("$BuildCommand" -eq "") {
-//             codeql database create \$DatabasePath --language "$Language" --source-root .
-//         } else {
-//             codeql database create \$DatabasePath --language "$Language" --source-root . --command "$BuildCommand"
-//         }
-//         Write-Output "Database initialized"
-//
-//         Write-Output "Analyzing database"
-//         codeql database analyze --download "\$DatabasePath" --sarif-category "$Language" --format sarif-latest --output "\$DatabasePath.sarif" "codeql/$Language-queries:codeql-suites/$Language-code-scanning.qls"
-//         Write-Output "Database analyzed"
-//
-//         Write-Output "Generating CSV of results"
-//         codeql database interpret-results "\$DatabasePath" --format=csv --output="codeql-scan-results.csv"
-//         Write-Output "CSV of results generated"
-//
-//         Write-Output "Uploading SARIF file"
-//         \$Commit = "\$(git rev-parse HEAD)"
-//         codeql github upload-results --repository "$Org/$Repo"  --ref "refs/heads/$Branch" --commit "\$Commit" --sarif="\$DatabasePath.sarif"
-//         Write-Output "SARIF file uploaded"
-//
-//         Write-Output "Generating Database Bundle"
-//         \$DatabaseBundle = "$Language-database.zip"
-//         codeql database bundle "\$DatabasePath" --output "\$DatabaseBundle"
-//         Write-Output "Database Bundle generated"
-//
-//         Write-Output "Uploading Database Bundle"
-//         \$Headers = @{
-//             "Content-Length" = "\$((Get-Item \$DatabaseBundle).Length)"
-//             "Authorization" = "token $token"
-//         }
-//         Invoke-RestMethod -ContentType 'application/zip' -Headers \$Headers -Method Post -InFile \$DatabaseBundle -Uri "https://uploads.github.com/repos/$Org/$Repo/code-scanning/codeql/databases/$Language?name=\$DatabaseBundle"
-//         Write-Output "Database Bundle uploaded"
-//     """
+    powershell """
+        if ("$Branch" -eq "")
+        {
+            $Branch = "\$((Write-Output \$env:GIT_BRANCH).split('/')[1])"
+        }
+
+        Write-Output "Initializing database"
+        \$DatabasePath = "$Repo-$Language"
+        if ("$BuildCommand" -eq "") {
+            codeql database create \$DatabasePath --language "$Language" --source-root .
+        } else {
+            codeql database create \$DatabasePath --language "$Language" --source-root . --command "$BuildCommand"
+        }
+        Write-Output "Database initialized"
+
+        Write-Output "Analyzing database"
+        codeql database analyze --download "\$DatabasePath" --sarif-category "$Language" --format sarif-latest --output "\$DatabasePath.sarif" "codeql/$Language-queries:codeql-suites/$Language-code-scanning.qls"
+        Write-Output "Database analyzed"
+
+        Write-Output "Generating CSV of results"
+        codeql database interpret-results "\$DatabasePath" --format=csv --output="codeql-scan-results.csv"
+        Write-Output "CSV of results generated"
+
+        Write-Output "Uploading SARIF file"
+        \$Commit = "\$(git rev-parse HEAD)"
+        codeql github upload-results --repository "$Org/$Repo"  --ref "refs/heads/$Branch" --commit "\$Commit" --sarif="\$DatabasePath.sarif"
+        Write-Output "SARIF file uploaded"
+
+        Write-Output "Generating Database Bundle"
+        \$DatabaseBundle = "$Language-database.zip"
+        codeql database bundle "\$DatabasePath" --output "\$DatabaseBundle"
+        Write-Output "Database Bundle generated"
+
+        Write-Output "Uploading Database Bundle"
+        \$Headers = @{
+            "Content-Length" = "\$((Get-Item \$DatabaseBundle).Length)"
+            "Authorization" = "\$Env:TokenHeader
+        }
+        Invoke-RestMethod -ContentType 'application/zip' -Headers \$Headers -Method Post -InFile \$DatabaseBundle -Uri "https://uploads.github.com/repos/$Org/$Repo/code-scanning/codeql/databases/$Language?name=\$DatabaseBundle"
+        Write-Output "Database Bundle uploaded"
+    """
 
 }
