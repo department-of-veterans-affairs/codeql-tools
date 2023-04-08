6e021eb12eb20b0dee1503551fffee28bb015659
Brett Logan <lindluni@github.com>
2023-04-08 10:26:24 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
@@ -1,4 +1,6 @@
 def call(org, repo, branch, language, buildCommand, token) {
+    env.GITHUB_TOKEN = token
+    env.TOKEN_HEADER = "Authorization: token $token"
     sh """
         if [[ -z "$branch" ]; then
             # This doesn't work if branch includes a slash in it
@@ -40,7 +42,7 @@ def call(org, repo, branch, language, buildCommand, token) {
         sizeInBytes=`stat --printf="%s" \$databaseBundle`
         curl --http1.0 --silent --retry 3 -X POST -H "Content-Type: application/zip" \
         -H "Content-Length: \$sizeInBytes" \
-        -H "Authorization: token $token" \
+        -H $TOKEN_HEADER \
         -T "\$databaseBundle" \
         "https://uploads.github.com/repos/$org/$repo/code-scanning/codeql/databases/$language?name=\$databaseBundle"
         echo "Database Bundle uploaded"
