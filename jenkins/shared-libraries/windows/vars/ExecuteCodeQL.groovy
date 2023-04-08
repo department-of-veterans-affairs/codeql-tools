39c1544cc194146a2f3f3f54db110d31990b811e
Brett Logan <lindluni@github.com>
2023-04-08 10:57:05 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
@@ -38,7 +38,7 @@ def call(Org, Repo, Branch, Language, BuildCommand, Token) {
         Write-Output "Uploading Database Bundle"
         \$Headers = @{
             "Content-Length" = "\$((Get-Item \$DatabaseBundle).Length)"
-            "Authorization" = "\$Env:TOKEN_HEADER"
+            "Authorization" = "\$Env:AUTHORIZATION_HEADER"
         }
         Invoke-RestMethod -ContentType 'application/zip' -Headers \$Headers -Method Post -InFile \$DatabaseBundle -Uri "https://uploads.github.com/repos/$Org/$Repo/code-scanning/codeql/databases/$Language?name=\$DatabaseBundle"
         Write-Output "Database Bundle uploaded"
