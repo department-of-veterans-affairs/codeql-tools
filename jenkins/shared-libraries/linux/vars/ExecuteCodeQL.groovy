7fab1f56612e7ae9ff7218be70a108eddb90040d
Brett Logan <lindluni@github.com>
2023-04-09 17:23:40 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
@@ -11,6 +11,7 @@ def call(org, repo, branch, language, buildCommand, token) {
     env.SARIF_FILE = sprintf("%s-%s.sarif", repo, language)
 
     sh """
+        set -x
         if [[ -z "$BRANCH" ]; then
             # This doesn't work if branch includes a slash in it
             echo "Branch is empty, using GIT_BRANCH"
