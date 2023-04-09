ac9013718fa369dd764ccd3a20d9f0278e8962de
Brett Logan <lindluni@github.com>
2023-04-09 17:28:02 -0400

    Update files
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
@@ -18,7 +18,7 @@ def call(org, repo, branch, language, buildCommand, token) {
         fi
 
         echo "Initializing database"
-        if [[ -z "$BUILD_COMMAND" ]]; then
+        if [[ "$BUILD_COMMAND" == "" ]]; then
             echo "No build command, using default"
             codeql database create "$DATABASE_PATH" --language="$LANGUAGE" --source-root .
         else
