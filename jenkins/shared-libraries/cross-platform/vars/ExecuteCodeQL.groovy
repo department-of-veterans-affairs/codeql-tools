a71f4d0e970843940b59f23b5ef622ba07da26e9
Brett Logan <lindluni@github.com>
2023-04-28 12:13:34 -0400

    Debug
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/cross-platform/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/cross-platform/vars/ExecuteCodeQL.groovy
@@ -40,7 +40,12 @@ def call(org, repo, branch, language, buildCommand, token, installCodeQL) {
     def url = sprintf("https://github.com/github/codeql-action/releases/download/%s/codeql-bundle-linux64.tar.gz", version)
     def downloadPath = sprintf("%s/codeql.tgz", env.WORKSPACE)
     println "Downloading CodeQL version ${version} from ${url} at ${downloadPath}"
-    downloadFile(url, "tmp/codeql.tgz")
+    downloadFile(url, "/tmp/codeql.tgz")
+
+    sh """
+       pwd
+       ls /tmp
+    """
 
     println "Extracting CodeQL bundle"
     extract("codeql.tgz", "/tmp/codeql")
