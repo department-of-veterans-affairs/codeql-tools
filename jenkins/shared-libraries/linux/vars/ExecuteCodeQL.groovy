ce71b29106c51966d4c021afd8183877ad1f9a7e
Brett Logan <lindluni@github.com>
2023-05-08 11:16:17 -0400

    Enable update
    Signed-off-by: Brett Logan <lindluni@github.com>

--- jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
@@ -74,13 +74,13 @@ def call(org, repo, branch, language, buildCommand, token, installCodeQL) {
                 tar -xf "${WORKSPACE}/codeql.tgz" --directory "${WORKSPACE}"
                 rm "${WORKSPACE}/codeql.tgz"
             else
-                id=\$(curl --silent --retry 3 --location \
+                id=\$(curl --retry 3 --location \
                 --header "${AUTHORIZATION_HEADER}" \
                 --header "Accept: application/vnd.github+json" \
                 "https://api.github.com/repos/github/codeql-action/releases/latest" | jq -r .tag_name)
 
                 echo "Downloading CodeQL version '\$id'"
-                curl --silent --retry 3 --location --output "${WORKSPACE}/codeql.tgz" \
+                curl --retry 3 --location --output "${WORKSPACE}/codeql.tgz" \
                 --header "${AUTHORIZATION_HEADER}" \
                 "https://github.com/github/codeql-action/releases/download/\$id/codeql-bundle-linux64.tar.gz"
                 tar -xf "${WORKSPACE}/codeql.tgz" --directory "${WORKSPACE}"
