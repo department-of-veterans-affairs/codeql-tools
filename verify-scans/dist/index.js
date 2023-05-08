8792c67876879c68ef699d76887b17c01a86a1e2
Brett Logan <lindluni@github.com>
2023-05-07 22:05:29 -0400

    Fix listing of languages
    Signed-off-by: Brett Logan <lindluni@github.com>

--- verify-scans/dist/index.js
+++ verify-scans/dist/index.js
@@ -65047,7 +65047,7 @@ const main = async () => {
                 }
 
                 core.info(`[${repository.name}]: Validating CodeQL CLI version`)
-                for(const version of analyses.versions) {
+                for (const version of analyses.versions) {
                     if (!codeQLVersions.includes(version)) {
                         core.warning(`[${repository.name}]: [out-of-date-cli] Outdated CodeQL CLI version found: ${version}`)
                         outdatedCLI.push(repository.name)
@@ -65218,7 +65218,7 @@ const getFile = async (octokit, owner, repo, path) => {
             path: path
         })
 
-        if(ENABLE_DEBUG) {
+        if (ENABLE_DEBUG) {
             core.info(`[TRACE] reusableWorkflowInUse: ${Buffer.from(response.data.content, 'base64').toString()}`)
         }
 
@@ -65239,7 +65239,7 @@ const getRawFile = async (octokit, owner, repo, path) => {
             path: path
         })
 
-        if(ENABLE_DEBUG) {
+        if (ENABLE_DEBUG) {
             core.info(`[TRACE] reusableWorkflowInUse: ${Buffer.from(response.data.content, 'base64').toString()}`)
         }
 
@@ -65254,8 +65254,6 @@ const getRawFile = async (octokit, owner, repo, path) => {
 
 const missingLanguages = (requiredLanguages, languages) => {
     try {
-        console.log(requiredLanguages)
-        console.log(languages)
         return requiredLanguages.filter(language => !languages.includes(language))
     } catch (e) {
         throw new Error(`Failed to calculate missing languages: ${e.message}`)
@@ -65271,9 +65269,10 @@ const listLanguages = async (octokit, owner, repo, ignoredLanguages) => {
 
         return Object.keys(languages).map(language => language.toLowerCase())
             .map(language => {
-                if(language === 'kotlin') {
+                if (language === 'kotlin') {
                     return 'java'
                 }
+                return language
             })
             .filter(language => supportedCodeQLLanguages.includes(language))
             .filter(language => !ignoredLanguages.includes(language))
@@ -65345,7 +65344,7 @@ const listCodeQLAnalyses = async (octokit, owner, repo, branch, range) => {
             versions.push(analysis.tool.version)
             const environment = JSON.parse(analysis.environment)
             let language = environment.language || analysis.category
-            if(language === 'kotlin') {
+            if (language === 'kotlin') {
                 language = 'java'
             }
             if (!languages.includes(language)) {
