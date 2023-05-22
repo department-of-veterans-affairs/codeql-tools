41cd1c723e210b524afaba42aa0636c18099181b
Brett Logan <lindluni@github.com>
2023-05-21 20:26:04 -0400

    Update log processing script
    Signed-off-by: Brett Logan <lindluni@github.com>

--- scripts/process-logs.sh
+++ scripts/process-logs.sh
@@ -37,7 +37,15 @@ for key in "${!workflows[@]}"; do
 
   echo "Moving log file"
   git status
-  echo "---\\nlayout: minimal\ntitle: ${key} Logs\nnav_order: 100\nparent: Code Scanning Governance Platform Dashboard\n---\n\n\`\`\`shell\n" > "logs/${key}.md"
+  echo "---
+layout: minimal
+title: ${key} Logs
+nav_order: 50
+parent: Code Scanning Governance Platform Dashboard
+---
+
+\`\`\`shell
+" > "logs/${key}.md"
   cat "${workflows[$key]}" >> "logs/${key}.md"
   echo "\`\`\`" >> "logs/${key}.md"
   git clean -ffd
