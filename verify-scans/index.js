e3c9746cb7cde8ef93c0f980ddf1f353dbbf41a7
Brett Logan <lindluni@github.com>
2023-05-21 20:01:05 -0400

    Fix table generation
    Signed-off-by: Brett Logan <lindluni@github.com>

--- verify-scans/index.js
+++ verify-scans/index.js
@@ -676,8 +676,8 @@ const updateFile = async (octokit, owner, repo, branch, path, message, content, 
 
 const createDashboardMarkdown = async() => {
     const table = markdownTable([
-        ['Fully Enabled', FULLY_COMPLIANT_REPOS.length],
-        ['Partially Enabled', CONFIGURED_MISSING_SCANS_REPOS.length],
+        ['Enabled', FULLY_COMPLIANT_REPOS.length],
+        ['Enabled: Non-Compliant', CONFIGURED_MISSING_SCANS_REPOS.length],
         ['Not Enabled', TOTAL_REPOS.length - FULLY_COMPLIANT_REPOS.length],
     ])
 
