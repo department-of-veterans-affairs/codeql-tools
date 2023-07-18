6a503d58fdfdc9328fc2524ad69c29c6f72709c0
Ari LiVigni <arilivigni@github.com>
2023-07-18 13:22:54 -0400

    fix path to config file

--- jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
@@ -9,7 +9,7 @@ def call(Org, Repo, Branch, Language, BuildCommand, Token, InstallCodeQL) {
         env.BRANCH = Branch
     }
     env.BUILD_COMMAND = BuildCommand
-    env.CONFIG_FILE = ".github\\codeql.yml"
+    env.CONFIG_FILE = ".github\\codeql-config.yml"
     env.DATABASE_BUNDLE = sprintf("%s-database.zip", Language)
     env.DATABASE_PATH = sprintf("%s-%s", Repo, Language)
     env.GITHUB_TOKEN = Token
