22cd41e09d93804496c1b8b9e9d212228fe669cf
Ari LiVigni <arilivigni@github.com>
2023-07-21 15:48:45 -0400

    quote all commands for codeql that was done installig codeql

--- jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/windows/vars/ExecuteCodeQL.groovy
@@ -90,13 +90,101 @@ def call(Org, Repo, Branch, Language, BuildCommand, Token, InstallCodeQL) {
         Write-Output "WORKSPACE: \$Env:WORKSPACE"
         Write-Output "Initializing database"
         Write-Output "CodeQL Config file: \$Env:CONFIG_FILE"
-        if (Test-Path -Path "\$Env:WORKSPACE\\codeql\\codeql") {
-            "Path exists!"
-            "\$Env:WORKSPACE\\codeql\\codeql database create \$Env:DATABASE_PATH --language \$Env:LANGUAGE --source-root ."
+
+        if (!(Test-Path "\$Env:CONFIG_FILE")) {
+            if ("\$Env:BUILD_COMMAND" -eq "") {
+                Write-Output "No build command specified, using default"
+                if("\$Env:INSTALL_CODEQL" -eq "true") {
+                    "\$Env:WORKSPACE\\codeql\\codeql database create \$Env:DATABASE_PATH --language \$Env:LANGUAGE --source-root ."
+                } else {
+                    codeql database create "\$Env:DATABASE_PATH" --language "\$Env:LANGUAGE" --source-root .
+                }
+            } else {
+                Write-Output "Build command specified, using '\$Env:BUILD_COMMAND'"
+                if("\$Env:INSTALL_CODEQL" -eq "true") {
+                    "\$Env:WORKSPACE\\codeql\\codeql database create \$Env:DATABASE_PATH --language \$Env:LANGUAGE --source-root . --command \$Env:BUILD_COMMAND"
+                } else {
+                    codeql database create "\$Env:DATABASE_PATH" --language "\$Env:LANGUAGE" --source-root . --command "\$Env:BUILD_COMMAND"
+                }
+            }
         } else {
-            "Path doesn't exist."
+            if ("\$Env:BUILD_COMMAND" -eq "") {
+                Write-Output "No build command specified, using default"
+                if("\$Env:INSTALL_CODEQL" -eq "true") {
+                    "\$Env:WORKSPACE\\codeql\\codeql database create \$Env:DATABASE_PATH --language \$Env:LANGUAGE --codescanning-config \$Env:CONFIG_FILE --source-root ."
+                } else {
+                    codeql database create "\$Env:DATABASE_PATH" --language "\$Env:LANGUAGE" --codescanning-config "\$Env:CONFIG_FILE" --source-root .
+                }
+            } else {
+                Write-Output "Build command specified, using '\$Env:BUILD_COMMAND'"
+                if("\$Env:INSTALL_CODEQL" -eq "true") {
+                    "\$Env:WORKSPACE\\codeql\\codeql database create \$Env:DATABASE_PATH --language \$Env:LANGUAGE --codescanning-config \$Env:CONFIG_FILE --source-root . --command \$Env:BUILD_COMMAND"
+                } else {
+                    codeql database create "\$Env:DATABASE_PATH" --language "\$Env:LANGUAGE" --codescanning-config "\$Env:CONFIG_FILE" --source-root . --command "\$Env:BUILD_COMMAND"
+                }
+            }
         }
-        
+        Write-Output "Database initialized"
+
+        Write-Output "Check if the current directory matches \$Env:WORKSPACE"
+        Write-Output "WORKSPACE: \$Env:WORKSPACE."
+        Write-Output "Current working directory: \$PWD."
+        if (\$Env:WORKSPACE -eq \$PWD) {
+            Write-Output "The current directory and \$Env:WORKSPACE match."
+            \$Env:CWD = ""
+            \$Env:SEP = ""
+        } else {
+            Write-Output "The current directory and \$Env:WORKSPACE do NOT match."
+            \$Env:CWD =  Split-Path \$Env:PWD -Leaf
+            \$Env:SEP = "-"
+        }
+        Write-Output "Check if the current directory matches \$Env:WORKSPACE"
+
+        Write-Output "Analyzing database"
+        if("\$Env:INSTALL_CODEQL" -eq "true") {
+            "\$Env:WORKSPACE\\codeql\\codeql database analyze --download \$Env:DATABASE_PATH --sarif-category ois-\$Env:LANGUAGE\$Env:SEP\$Env:CWD --format sarif-latest --output \$Env:SARIF_FILE \$Env:QL_PACKS"
+        } else {
+            codeql database analyze --download "\$Env:DATABASE_PATH" --sarif-category "ois-\$Env:LANGUAGE" --format sarif-latest --output "\$Env:SARIF_FILE" "\$Env:QL_PACKS"
+        }
+        Write-Output "Database analyzed"
+
+        Write-Output "Generating CSV of results"
+        if("\$Env:INSTALL_CODEQL" -eq "true") {
+            "\$Env:WORKSPACE\\codeql\\codeql database interpret-results \$Env:DATABASE_PATH --format=csv --output=codeql-scan-results-\$Env:LANGUAGE.csv \$Env:QL_PACKS"
+        } else {
+            codeql database interpret-results "\$Env:DATABASE_PATH" --format=csv --output="codeql-scan-results-\$Env:LANGUAGE.csv" "\$Env:QL_PACKS"
+        }
+        Write-Output "CSV of results generated"
+
+        if("\$Env:UPLOAD_RESULTS" -eq "true") {
+            Write-Output "Uploading SARIF file"
+            \$Commit = "\$(git rev-parse HEAD)"
+            if("\$Env:INSTALL_CODEQL" -eq "true") {
+                "\$Env:WORKSPACE\\codeql\\codeql github upload-results --repository \$Env:ORG/\$Env:REPO  --ref \$Env:REF --commit \$Commit --sarif=\$Env:SARIF_FILE"
+            } else {
+                codeql github upload-results --repository "\$Env:ORG/\$Env:REPO"  --ref "\$Env:REF" --commit "\$Commit" --sarif="\$Env:SARIF_FILE"
+            }
+            Write-Output "SARIF file uploaded"
+        }
+
+        Write-Output "Generating Database Bundle"
+        \$DatabaseBundle = "\$Env:DATABASE_BUNDLE"
+        if("\$Env:INSTALL_CODEQL" -eq "true") {
+            "\$Env:WORKSPACE\\codeql\\codeql database bundle \$Env:DATABASE_PATH --output \$Env:DATABASE_BUNDLE"
+        } else {
+            codeql database bundle "\$Env:DATABASE_PATH" --output "\$Env:DATABASE_BUNDLE"
+        }
+        Write-Output "Database Bundle generated"
+
+        if("\$Env:UPLOAD_RESULTS" -eq "true") {
+            Write-Output "Uploading Database Bundle"
+            \$Headers = @{
+                "Content-Length" = "\$((Get-Item \$Env:DATABASE_BUNDLE).Length)"
+                "Authorization" = "\$Env:AUTHORIZATION_HEADER"
+            }
+            Invoke-RestMethod -ContentType "application/zip" -Headers \$Headers -Method Post -InFile "\$Env:DATABASE_BUNDLE" -Uri "\$Env:UPLOAD_URL"
+            Write-Output "Database Bundle uploaded"
+        }
     """
 }
 
