57db94c7308d566b07bded5dea212a285c926aa6
Ari LiVigni <arilivigni@github.com>
2023-07-25 10:34:28 -0400

    echo sarif category

--- jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
+++ jenkins/shared-libraries/linux/vars/ExecuteCodeQL.groovy
@@ -154,18 +154,19 @@ def call(org, repo, branch, language, buildCommand, token, installCodeQL) {
         echo "Check if PWD matches WORKSPACE"
         if [ "${PWD}" != "${WORKSPACE}" ]; then
             SUBDIR=\$( echo ${PWD} | awk -F'/' '{print \$NF}' )
-            SUBDIR="-\${SUBDIR}"
+            SUBDIR="\${SUBDIR}"
             SEP='-'
         else
             SUBDIR=''
             SEP=''
         fi
+        echo "Sarif Category: ${LANGUAGE}\${SEP}\${SUBDIR}"
 
         echo "Analyzing database"
         if [ "${INSTALL_CODEQL}" = true ]; then
             ./codeql/codeql database analyze "${DATABASE_PATH}" --no-download --sarif-category "ois-${LANGUAGE}\${SEP}\${SUBDIR}" --format sarif-latest --output "${SARIF_FILE}" "${QL_PACKS}"
         else
-            codeql database analyze "${DATABASE_PATH}" --no-download --sarif-category "ois-${LANGUAGE}\${SUBDIR}" --format sarif-latest --output "${SARIF_FILE}" "${QL_PACKS}"
+            codeql database analyze "${DATABASE_PATH}" --no-download --sarif-category "ois-${LANGUAGE}\${SEP}\${SUBDIR}" --format sarif-latest --output "${SARIF_FILE}" "${QL_PACKS}"
         fi
         echo "Database analyzed"
 
